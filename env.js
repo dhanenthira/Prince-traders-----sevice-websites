e8fa34ccc23c72dc75dade02a8e02077a96ce480
Dhanenthira M <dhanenthira2005@example.com>
2026-03-06 20:23:48 +0530

    Include env.js and fix image path

---
+++ env.js
@@ -0,0 +1,9 @@
+window.env = {
+    API_KEY: "AIzaSyBEGlxRT7NeIsDmrZ4TozPJXYJs1a8T6ok",
+    AUTH_DOMAIN: "prince-1d4ed.firebaseapp.com",
+    PROJECT_ID: "prince-1d4ed",
+    STORAGE_BUCKET: "prince-1d4ed.firebasestorage.app",
+    MESSAGING_SENDER_ID: "719932262721",
+    APP_ID: "1:719932262721:web:f85bd29a3a76fc39d139c0",
+    MEASUREMENT_ID: "G-Q7RS7F7B9Q"
+};
